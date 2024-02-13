8f712f6c6952021055a9afe391e16a0066002c25
lamia49 <80590112+lamia49@users.noreply.github.com>
2024-02-13 22:31:29 +0300

    Add files via upload

---
+++ execis3/src/Main.java
@@ -0,0 +1,132 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Qustion1
+
+     /*  for (int i = 1; i <= 100; i++) {
+            if (i%3==0&&i%5==0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i%3==0){
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println("Number: " + i);
+            }*/
+
+        //Qustion2
+
+    /*
+        String Sentence = input.nextLine();
+        ;
+        String reverse = "";
+        for (int i = Sentence.length()-1; i >= 0; i--) {
+            reverse += Sentence.charAt(i);
+        }
+        System.out.println(reverse);*/
+
+//        //Qustion3
+/*
+        System.out.println("Enter Number:");
+        int number= input.nextInt();
+        int fact=1;
+        for(int i=1;i<=number;i++){
+            fact=i*fact;
+        }
+        System.out.println("The factorial of "+number+" is :"+fact);*/
+
+
+        //Qustion4
+
+
+       /* System.out.print("Enter The  number: ");
+        int num1= input.nextInt();
+        System.out.print("Enter The Pwoer number: ");
+        int num2= input.nextInt();
+        int j=1;
+       for(int i=1;i<=num2;i++){
+           j=j*num1;
+       }
+       System.out.print("The Power number of "+num1+" is "+j);*/
+
+        //Qution5:
+
+
+     /*   int odd=0;
+        int even=0;
+         for(int i=0;i<=10;i++){
+             if(i%2==0){
+                 even=i+even;
+             }else {
+                 odd=odd+i;
+             }
+         }
+         System.out.println("sum of odd is: "+odd);
+         System.out.println("sum of even is: "+even);
+        System.out.println("sum of odd and even is: "+(even+odd));*/
+
+
+        //Qustion6
+
+//
+//            System.out.print("Enter only Postive Number: ");
+//               int nember3 = input.nextInt();
+//               if(nember3<1){
+//                   System.out.print("Error only Postive Number");
+//                   System.exit(0);
+//               }
+//                    boolean Check=true;
+//                    int Store;
+//                    for(int i=2;i<=nember3/2;i++){
+//                        Store=nember3%i;
+//                        if(Store==0) {
+//                            Check = false;
+//                            break;
+//                        }
+//                    }
+//                       if(Check){
+//                          System.out.print("  The number is prim");
+//                      }else{
+//                          System.out.print("The number is Not prim");
+//                      }
+
+
+        //Qustion7
+
+
+/*        int Week=1;
+        do{
+            System.out.println("Week"+Week);
+        for(int i=1;i<=7;i++){
+            System.out.println("Day"+i);
+        }Week++;
+        }while (Week<=4);*/
+
+
+        //Qustion8
+        
+
+//        System.out.print("Enter Word To cheak: ");
+//        String Word1 = input.nextLine();
+//        String Revrsed = "";
+//        for (int i = Word1.length() - 1; i >= 0; i--) {
+//            Revrsed += Word1.charAt(i);
+//        }
+//        if (Word1.equalsIgnoreCase(Revrsed)) {
+//           System.out.print("The Word is Palindrome");
+//          }else {
+//            System.out.print("The Word is Not Palindrome");
+//        }
+
+
+    }}
+
+
+
+
